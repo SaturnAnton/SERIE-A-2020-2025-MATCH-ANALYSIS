6778dc3b8cc66d0610dcb0fd99faa0eaf266e8c9
SaturnAnton <157697504+SaturnAnton@users.noreply.github.com>
2025-09-29 20:39:29 +0200

    Modifiche al codice e all'interfaccia grafica
    Risolto il problema del percorso del file e fatte alcune modifiche per quanto riguarda l'interfaccia web

--- code/seriea_analisi.py
+++ code/seriea_analisi.py
@@ -1,27 +1,25 @@
-def openFile(filename):
-   result = open(filename,'r')
-   return result
+import os
 
+def openFile(filename):
+    base_path = os.path.dirname(os.path.abspath(__file__))
+    filepath = os.path.join(base_path, "..", "data", filename)
+    return open(filepath, "r")
 
 def closeFile(fil):
-   fil.flush()
-   fil.close()
-   return "ok"
+    fil.close()
+    return "ok"
 
-
-def stampaPartite (sc,sa):
-    file = "data/matches_serie_A.csv"
-    f = openFile(file)
+def stampaPartite(sc, sa):
+    f = openFile("matches_serie_A.csv")
     
     riga = f.readline()
-
-    while riga != "" :
+    while riga != "":
         new = riga
         i = 0
 
-        while i < 10 :
+        while i < 10:
             pos = new.find(',')
-            if i == 1 :
+            if i == 1:
                 data = new[0:pos:1]
             elif i == 6:
                 posto = new[0:pos:1]
@@ -30,40 +28,39 @@ def stampaPartite (sc,sa):
             elif i == 9:
                 gol_avversari = new[0:pos:1]
             new = new[pos+1::1]
-            i = i+1
+            i += 1
+
         pos = new.find(',')
         squadra_avversaria = new[0:pos:1]
         
-        while i < 28 :
+        while i < 28:
             pos = new.find(',')
             new = new[pos+1::1]
-            i = i+1
+            i += 1
 
         squadra_scelta = new.strip()
 
-        if (sc == squadra_scelta and sa == squadra_avversaria) :
-            if(posto == 'Away') :
-                print(data + " " + squadra_avversaria + "-" + squadra_scelta + " Punteggio finale: " + gol_avversari + "-" + gol_squadra)
+        if sc == squadra_scelta and sa == squadra_avversaria:
+            if posto == 'Away':
+                print(f"{data} {squadra_avversaria}-{squadra_scelta} Punteggio finale: {gol_avversari}-{gol_squadra}")
             else:
-                print(data + " " + squadra_scelta + "-" + squadra_avversaria + " Punteggio finale: " + gol_squadra + "-" + gol_avversari)
+                print(f"{data} {squadra_scelta}-{squadra_avversaria} Punteggio finale: {gol_squadra}-{gol_avversari}")
 
         riga = f.readline()
 
     closeFile(f)
 
-def stampaPartitaSingola(sc,sa,stagione) :
-    file = "data/matches_serie_A.csv"
-    f = openFile(file)
+def stampaPartitaSingola(sc, sa, stagione):
+    f = openFile("matches_serie_A.csv")
     
     riga = f.readline()
-
-    while riga != "" :
+    while riga != "":
         new = riga
         i = 0
 
-        while i < 10 :
+        while i < 10:
             pos = new.find(',')
-            if i == 1 :
+            if i == 1:
                 data = new[0:pos:1]
             elif i == 6:
                 posto = new[0:pos:1]
@@ -72,21 +69,22 @@ def stampaPartitaSingola(sc,sa,stagione) :
             elif i == 9:
                 gol_avversari = new[0:pos:1]
             new = new[pos+1::1]
-            i = i+1
+            i += 1
+
         pos = new.find(',')
         squadra_avversaria = new[0:pos:1]
         
-        while i < 28 :
+        while i < 28:
             pos = new.find(',')
-            if i == 27 :
+            if i == 27:
                 s = new[0:pos:1]
             new = new[pos+1::1]
-            i = i+1
+            i += 1
 
         squadra_scelta = new.strip()
 
         if sc == squadra_scelta and sa == squadra_avversaria and s == stagione and posto == 'Home':
-            print(data + " " + squadra_scelta + "-" + squadra_avversaria + " Punteggio finale: " + gol_squadra + "-" + gol_avversari)
+            print(f"{data} {squadra_scelta}-{squadra_avversaria} Punteggio finale: {gol_squadra}-{gol_avversari}")
 
         riga = f.readline()
 
@@ -94,21 +92,21 @@ def stampaPartitaSingola(sc,sa,stagione) :
 
 
 print("Seleziona l'azione che vuoi eseguire")
-print("1 - Guarda gli ultimi confronti tra le due squadre selezionate\n2 - Dimmi quanto è finita una determinata partita selezioanata")
+print("1 - Guarda gli ultimi confronti tra le due squadre selezionate\n2 - Dimmi quanto è finita una determinata partita selezionata")
 select = int(input())
 
-while(select!=1 and select!=2):
+while select != 1 and select != 2:
     print("Il numero selezionato non è giusto. Riprova")
     select = int(input())
 
-if(select == 1):
+if select == 1:
     print("Inserisci la prima squadra")
     sq1 = input()
 
     print("Inserisci la seconda squadra")
     sq2 = input()
 
-    stampaPartite(sq1,sq2)
+    stampaPartite(sq1, sq2)
 else:
     print("Inserisci la squadra di casa")
     sq1 = input()
@@ -119,4 +117,4 @@ else:
     print("Seleziona la stagione (N.B Nel mettere la stagione si conta la seconda data)\n Esempio: Stagione 2020-2021 --> 2021")
     season = input()
 
-    stampaPartitaSingola(sq1,sq2,season)
+    stampaPartitaSingola(sq1, sq2, season)
